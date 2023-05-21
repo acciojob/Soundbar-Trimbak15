bbbb7ca8c10fa0b7d0f942b9922b6bea5e4ddeb6
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-05-21 18:54:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 function playSound(soundFile) {
   const audioElement = document.getElementById('audio');
-  audioElement.src = `sounds/${soundFile}`;
-  audioElement.play();
+
+  // Clear any existing source elements
+  while (audioElement.firstChild) {
+    audioElement.removeChild(audioElement.firstChild);
+  }
+
+  const sourceElement = document.createElement('source');
+  sourceElement.src = `sounds/${soundFile}`;
+  audioElement.appendChild(sourceElement);
+
+  audioElement.load();
+  audioElement.play()
+    .catch((error) => {
+      console.error(error);
+      alert('Failed to play the audio. Please check the file format and location.');
+    });
 }
 
 function stopSound() {

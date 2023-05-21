5bd63f61d59c4c68af21dc1e1a83d23eb5d75942
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-05-21 18:41:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function playSound(soundFile) {
+  const audio = new Audio(`sounds/${soundFile}`);
+  audio.play();
+}
+
+function stopSound() {
+  const audioElements = document.getElementsByTagName('audio');
+  for (let i = 0; i < audioElements.length; i++) {
+    audioElements[i].pause();
+    audioElements[i].currentTime = 0;
+  }
+}
